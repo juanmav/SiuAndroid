ff42875cff0ac5f97d2a03473bd9b2f1a0b2fa37
JuanMa <juanma.v82@gmail.com>
2013-11-08 17:03:50 -0300

    [#89] Obtención de imagenes desde memoria.

--- src/com/diphot/siu/views/FotoSelection.java
+++ src/com/diphot/siu/views/FotoSelection.java
@@ -1,19 +1,30 @@
 package com.diphot.siu.views;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.diphot.siu.R;
 import com.diphot.siu.SiuConstants;
 import com.diphot.siu.util.Util;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.view.Menu;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 
 public class FotoSelection extends Activity {
 
@@ -21,10 +32,12 @@ public class FotoSelection extends Activity {
 	private ImageView imageView1;
 	private ImageView imageView2;
 	private ImageView imageView3;
-	private Bitmap bm1;
-	private Bitmap bm2;
-	private Bitmap bm3;
 	private Boolean picturetaken = false;
+	private Boolean image1Take = false;
+	private Boolean image2Take = false;
+	private Boolean image3Take = false;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,8 +48,9 @@ public class FotoSelection extends Activity {
 		OnClickListener o = new OnClickListener(){
 			@Override
 			public void onClick(View view) {
-				Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
-				startActivityForResult(cameraIntent, CAMERA_REQUEST + view.getId()); 
+				/*Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+				startActivityForResult(cameraIntent, CAMERA_REQUEST + view.getId());*/
+				openImageIntent(CAMERA_REQUEST + view.getId());
 			}
 		};
 		this.imageView1.setOnClickListener(o);
@@ -44,7 +58,7 @@ public class FotoSelection extends Activity {
 		this.imageView3.setOnClickListener(o);
 		// Inicializo el bundle para poner las fotos.
 	}
-	
+
 	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -55,18 +69,26 @@ public class FotoSelection extends Activity {
 	// Guarda las imagenes si por algun motivo se destruye y se vuelve a crear
 	// la Activity. Es suele suceder en las rotaciones de pantalla.
 	public void onSaveInstanceState(Bundle savedInstanceState) {
-		savedInstanceState.putParcelable(SiuConstants.IMG1_PROPERTY, bm1);
-		savedInstanceState.putParcelable(SiuConstants.IMG2_PROPERTY, bm2);
-		savedInstanceState.putParcelable(SiuConstants.IMG3_PROPERTY, bm3);
+		savedInstanceState.putParcelable(SiuConstants.IMG1_PROPERTY, ((BitmapDrawable)imageView1.getDrawable()).getBitmap());
+		savedInstanceState.putParcelable(SiuConstants.IMG2_PROPERTY, ((BitmapDrawable)imageView2.getDrawable()).getBitmap());
+		savedInstanceState.putParcelable(SiuConstants.IMG3_PROPERTY, ((BitmapDrawable)imageView3.getDrawable()).getBitmap());
 		// al final llamamos a la super clase.
 		super.onSaveInstanceState(savedInstanceState);  
 	}  
 
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-		bm1 = savedInstanceState.getParcelable(SiuConstants.IMG1_PROPERTY);
-		bm2 = savedInstanceState.getParcelable(SiuConstants.IMG2_PROPERTY);
-		bm3 = savedInstanceState.getParcelable(SiuConstants.IMG3_PROPERTY);
+		Bitmap bm1 = null;
+		Bitmap bm2 = null;
+		Bitmap bm3 = null;
+
+		if (image1Take)
+			bm1 = savedInstanceState.getParcelable(SiuConstants.IMG1_PROPERTY);
+		if (image2Take)
+			bm2 = savedInstanceState.getParcelable(SiuConstants.IMG2_PROPERTY);
+		if (image3Take)
+			bm3 = savedInstanceState.getParcelable(SiuConstants.IMG3_PROPERTY);
+
 		if (bm1 != null){
 			this.picturetaken = true;
 			imageView1.setImageBitmap(bm1);
@@ -85,30 +107,30 @@ public class FotoSelection extends Activity {
 			imageView3.getLayoutParams().height = 250;
 			imageView3.getLayoutParams().width = 250;
 		}
-
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
-		// TODO hacer tres fotos distintas.
 		if (resultCode == RESULT_OK) {
 			this.picturetaken = true;
-			Bitmap bm = (Bitmap) data.getExtras().get("data"); 
+			if (data != null){
+				this.outputFileUri = data.getData();
+			}
 			if (requestCode == CAMERA_REQUEST + this.imageView1.getId()){
-				bm1 = bm;
-				imageView1.setImageBitmap(bm1);
 				imageView1.getLayoutParams().height = 250;
 				imageView1.getLayoutParams().width = 250;
+				imageView1.setImageURI(outputFileUri);
+				this.image1Take = true;
 			} else if (requestCode == CAMERA_REQUEST + this.imageView2.getId()){
-				bm2 = bm;
-				imageView2.setImageBitmap(bm2);
 				imageView2.getLayoutParams().height = 250;
 				imageView2.getLayoutParams().width = 250;
+				imageView2.setImageURI(outputFileUri);
+				this.image2Take = true;
 			} else if (requestCode == CAMERA_REQUEST + this.imageView3.getId()){
-				bm3 = bm;
-				imageView3.setImageBitmap(bm3);
 				imageView3.getLayoutParams().height = 250;
 				imageView3.getLayoutParams().width = 250;
+				imageView3.setImageURI(outputFileUri);
+				this.image3Take = true;
 			}
 		}  
 	}
@@ -117,15 +139,18 @@ public class FotoSelection extends Activity {
 		if (validateForm()){
 			Intent returnIntent = new Intent();
 			Bundle bundle = new Bundle();
-			bundle.putString(SiuConstants.IMG1_PROPERTY, Util.getEncodedImage(bm1));
-			bundle.putString(SiuConstants.IMG2_PROPERTY, Util.getEncodedImage(bm2));
-			bundle.putString(SiuConstants.IMG3_PROPERTY, Util.getEncodedImage(bm3));
+			if (image1Take)
+				bundle.putString(SiuConstants.IMG1_PROPERTY, Util.getEncodedImage(((BitmapDrawable)imageView1.getDrawable()).getBitmap()));
+			if (image2Take)
+				bundle.putString(SiuConstants.IMG2_PROPERTY, Util.getEncodedImage(((BitmapDrawable)imageView2.getDrawable()).getBitmap()));
+			if (image3Take)
+				bundle.putString(SiuConstants.IMG3_PROPERTY, Util.getEncodedImage(((BitmapDrawable)imageView3.getDrawable()).getBitmap()));
 			returnIntent.putExtras(bundle);
 			setResult(RESULT_OK,returnIntent);        
 			finish();
 		}
 	}
-	
+
 	private Boolean validateForm(){
 		if (!picturetaken){ // No se tomo foto.
 			new AlertDialog.Builder(FotoSelection.this)
@@ -141,5 +166,43 @@ public class FotoSelection extends Activity {
 		}
 		return picturetaken;
 	}
-	
+
+	Uri outputFileUri; 
+	private void openImageIntent(int requestCode) {
+
+		// Determine Uri of camera image to save.
+		final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MUTIMAGES" + File.separator);
+		root.mkdirs();
+		final String fname = "img_"+ System.currentTimeMillis() + ".jpg";
+		final File sdImageMainDirectory = new File(root, fname);
+		outputFileUri = Uri.fromFile(sdImageMainDirectory);
+
+		// Camera.
+		final List<Intent> cameraIntents = new ArrayList<Intent>();
+		final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		final PackageManager packageManager = getPackageManager();
+		final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+		for(ResolveInfo res : listCam) {
+			final String packageName = res.activityInfo.packageName;
+			final Intent intent = new Intent(captureIntent);
+			intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+			intent.setPackage(packageName);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+			cameraIntents.add(intent);
+		}
+
+		// Filesystem.
+		final Intent galleryIntent = new Intent();
+		galleryIntent.setType("image/*");
+		galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+		// Chooser of filesystem options.
+		final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
+
+		// Add the camera options.
+		chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[]{}));
+
+		startActivityForResult(chooserIntent, requestCode);
+	}
+
 }
